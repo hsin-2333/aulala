84a9a6a97fdc00daee74dd768e5433cce7c505d3
Sylvie <yuanqi1368@example.com>
2024-09-11 22:12:11 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="p-4 bg-blue-500 text-white">
+        <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
