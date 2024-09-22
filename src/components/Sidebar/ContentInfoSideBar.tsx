eb7efefa801695616bf9792aea50e3ae9851c246
Sylvie <yuanqi1368@example.com>
2024-09-22 23:07:50 +0800

    feat: fetch voice actors' avatars on InfoSideBar

--- src/components/Sidebar/ContentInfoSideBar.tsx
+++ src/components/Sidebar/ContentInfoSideBar.tsx
@@ -1,5 +1,12 @@
 import { RecentPlayContext } from "../../context/RecentPlayContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import dbApi from "../../utils/firebaseService";
+
+type UserWithAvatar = {
+  id: string;
+  userName: string;
+  avatar: string;
+};
 
 const ContentInfoSideBar = () => {
   const context = useContext(RecentPlayContext);
@@ -11,6 +18,29 @@ const ContentInfoSideBar = () => {
   const tags = storyInfo?.tags || [];
   const title = storyInfo?.title || "";
   const summary = storyInfo?.summary || "";
+  const [avatars, setAvatars] = useState<{ userName: string; avatar: string }[]>([]);
+
+  useEffect(() => {
+    const fetchAvatars = async () => {
+      if (storyInfo?.voice_actor) {
+        const avatarPromises = storyInfo.voice_actor.map(async (userName: string) => {
+          const condition = { userName };
+          const users = await dbApi.queryCollection("users", condition);
+          if (users.length > 0) {
+            const user = users[0] as UserWithAvatar;
+            console.log("user", user);
+            return { userName: user.userName, avatar: user.avatar };
+          }
+          return { userName, avatar: "" };
+        });
+
+        const avatars = await Promise.all(avatarPromises);
+        setAvatars(avatars);
+      }
+    };
+
+    fetchAvatars();
+  }, [storyInfo]);
 
   return (
     <div className="pb-12">
@@ -30,13 +60,12 @@ const ContentInfoSideBar = () => {
           <div className="flex m-2 px-4  bg-white-200  h-[1px] w-full"></div>
           <h2 className="text-left mb-2 px-4 text-lg font-semibold tracking-tight">Attendees</h2>
           <div className="text-left mb-2 px-4  flex flex-row gap-4 justify-start">
-            <img
-              src={storyInfo?.img_url ? storyInfo.img_url[0] : ""}
-              alt={`Cover for ${storyInfo?.title}`}
-              className="w-8 h-8 rounded-full"
-            />
-
-            <div className="text-left text-sm font-semibold tracking-tight leading-7">{storyInfo?.voice_actor}</div>
+            {avatars.map((attendee, index) => (
+              <div key={index} className="flex items-center gap-2">
+                <img src={attendee.avatar} alt={`Avatar of ${attendee.userName}`} className="w-8 h-8 rounded-full" />
+                <div className="text-left text-sm font-semibold tracking-tight leading-7">{attendee.userName}</div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
