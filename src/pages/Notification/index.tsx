7ec9ac3d36c05ad4dcbfd0c1962ae3692f0046b4
Sylvie <yuanqi1368@gmail.com>
2024-10-31 20:27:34 +0800

    chore: delete unused package

--- src/pages/Notification/index.tsx
+++ src/pages/Notification/index.tsx
@@ -1,6 +1,5 @@
 import { Divider } from "@nextui-org/divider";
 import { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import empty_notification from "../../assets/empty_notification.png";
 import { AuthContext } from "../../context/AuthContext";
 import dbApi from "../../utils/firebaseService";
@@ -22,7 +21,11 @@ const Notifications = () => {
     const fetchNotifications = async () => {
       if (user && user.userName) {
         try {
-          const userNotifications = await dbApi.queryCollection("notifications", { recipient: user.userName }, 10);
+          const userNotifications = await dbApi.queryCollection(
+            "notifications",
+            { recipient: user.userName },
+            10,
+          );
           setNotifications(userNotifications as Notification[]);
         } catch (error) {
           console.error("Error fetching notifications:", error);
@@ -38,31 +41,36 @@ const Notifications = () => {
     <div>
       {notifications.length > 0 ? (
         notifications.map((notification, index) => (
-          <div key={index} className="flex flex-1 flex-col p-4 ">
-            <div className="flex items-start ">
+          <div key={index} className="flex flex-1 flex-col p-4">
+            <div className="flex items-start">
               <div className="flex items-start gap-4 text-sm">
                 <div className="grid gap-1 text-left">
-                  <div className="font-semibold">🥰 {notification.title || "A script invitation"}</div>
-                  <div className="line-clamp-1 text-xs">{notification.message}</div>
+                  <div className="font-semibold">
+                    🥰 {notification.title || "A script invitation"}
+                  </div>
                   <div className="line-clamp-1 text-xs">
-                    <Link to={notification.link} className="text-xs text-blue-500">
-                      View
-                    </Link>
+                    {notification.message}
                   </div>
                 </div>
               </div>
               {notification.created_at && (
-                <div className="ml-auto text-xs text-muted-foreground">
-                  {new Date(notification.created_at.seconds * 1000).toLocaleString()}{" "}
+                <div className="text-muted-foreground ml-auto text-xs">
+                  {new Date(
+                    notification.created_at.seconds * 1000,
+                  ).toLocaleString()}
                 </div>
               )}
             </div>
-            <Divider className="my-6 pl-4 bg-default-200" />
+            <Divider className="my-6 bg-default-200 pl-4" />
           </div>
         ))
       ) : (
-        <div className="h-3/4 w-full p-8 text-center ">
-          <img src={empty_notification} alt="No notifications" className="mx-auto mt-8  mb-1 h-40" />
+        <div className="h-3/4 w-full p-8 text-center">
+          <img
+            src={empty_notification}
+            alt="No notifications"
+            className="mx-auto mb-1 mt-8 h-40"
+          />
           <span className="text-default-400">No Notifications Yet</span>
         </div>
       )}
